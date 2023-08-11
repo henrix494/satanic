80f717184b66c2c23b0a8fb42aac60261f136348
henrix49 <henrix494@gmail.com>
2023-08-11 18:22:58 +0300

    added z credit

--- server/routes/shipInfo.js
+++ server/routes/shipInfo.js
@@ -106,7 +106,7 @@ async function createShipingInfo(req, res) {
 				);
 
 				const zCreditData = await zCreditResponse.json();
-				res.status(200).json({ sessionUrl: zCreditData });
+				res.status(200).json(zCreditData);
 			} catch (error) {
 				console.error(error);
 				res.status(500).json({ error: "An error occurred." });
