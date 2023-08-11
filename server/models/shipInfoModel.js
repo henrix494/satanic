27d909bf733343bfa9749f19dc566fe9206bb6f4
henrix49 <henrix494@gmail.com>
2023-08-11 18:29:01 +0300

    added z credit

--- server/models/shipInfoModel.js
+++ server/models/shipInfoModel.js
@@ -3,18 +3,15 @@ const { model, Schema, models } = require(`mongoose`);
 const shipInfoSchema = new Schema({
 	email: {
 		type: String,
-		required: [true, "אימייל זה שדה חובה"],
 	},
 	name: {
 		type: String,
-		required: [true, "שם זה שדה חובה"],
 	},
 	lastName: {
 		type: String,
 	},
 	city: {
 		type: Array,
-		required: [true, " עיר זה שדה חובה"],
 	},
 	street: {
 		type: Array,
@@ -24,7 +21,6 @@ const shipInfoSchema = new Schema({
 	},
 	phone: {
 		type: String,
-		required: [true, " מספר זה שדה חובה  "],
 	},
 });
 const shipInfo = models.shipInfo || model("shipInfo", shipInfoSchema);
