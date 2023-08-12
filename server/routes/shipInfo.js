b010faec35d34e910bb8e6c1ae78664af2c68e5b
henrix494 <henrix494@gmail.com>
2023-08-12 21:12:40 +0300

    asc

--- server/routes/shipInfo.js
+++ server/routes/shipInfo.js
@@ -49,7 +49,7 @@ async function createShipingInfo(req, res) {
 					Amount: item.price,
 					Currency: "ILS",
 					Name: item.title,
-					Description: "מכונת קפה ידנית לחווית קפה מושלמת",
+					Description: item.des,
 					Quantity: item.que,
 					Image: item.img[0], // Assuming img is an array, use the first URL
 					IsTaxFree: "false",
